8828ea62507e4c542609b937b901a831038254fb
Chris Cooke <azoicaxiom@gmail.com>
2016-03-23 01:37:37 -0400

    Legendary Helms
    Added in initial array of legendary helm stats. Specific stats may be
    added or removed as scope of the project changes.

---
+++ js/legendaryHelms.js
@@ -0,0 +1,466 @@
+var json = {
+	"helms": [
+  	{
+      "name": "Maw of Despair",
+      "quality": "legendary",
+      "attributes": [
+      	"armor": 922,
+        "health": 605,
+        "offensive ability": 0,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Dread-Mask of Gurgoth",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 477,
+        "health": 0,
+        "offensive ability": 0,
+        "defensive ability": 16,
+        "elemental resist": 22,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Whisperer of Secrets",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 730,
+        "health": 0,
+        "offensive ability": 42,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 25,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Abyssal Mask",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 548,
+        "health": 0,
+        "offensive ability": 0,
+        "defensive ability": 28,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Faceguard of Justice",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 1060,
+        "health": 560,
+        "offensive ability": 0,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 22,
+        "pierce resist": 38,
+        "bleed resist": 28,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Beastcaller's Cowl",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 730,
+        "health": 220,
+        "offensive ability": 33,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Clairvoyant's Hat",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 548,
+        "health": 0,
+        "offensive ability": 0,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 18,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Circlet of the Great Serpent",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 598,
+        "health": 0,
+        "offensive ability": 33,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 26,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Mask of Infernal Truth",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 1154,
+        "health": 730,
+        "offensive ability": 0,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Mask of the Harbinger",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 796,
+        "health": 0,
+        "offensive ability": 32,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 15,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Demonslayer's Hat",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 796,
+        "health": 0,
+        "offensive ability": 0,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 18,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Markovian's Visor",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 1154,
+        "health": 0,
+        "offensive ability": 0,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 50,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Ulzuin's Headguard",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 796,
+        "health": 0,
+        "offensive ability": 28,
+        "defensive ability": 0,
+        "elemental resist": 18,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 20,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Hood of Dreeg",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 598,
+        "health": 0,
+        "offensive ability": 29,
+        "defensive ability": 0,
+        "elemental resist": 26,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Iskandra's Hood",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 670,
+        "health": 0,
+        "offensive ability": 0,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 25,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Ultos' Hood",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 796,
+        "health": 750,
+        "offensive ability": 0,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 40,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 25,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 5
+      ]
+    }
+    {
+      "name": "Crown of the Winter King",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 796,
+        "health": 0,
+        "offensive ability": 0,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Trozan's Hat",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 598,
+        "health": 0,
+        "offensive ability": 0,
+        "defensive ability": 12,
+        "elemental resist": 28,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 15,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Valdun's Hat",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 796,
+        "health": 450,
+        "offensive ability": 0,
+        "defensive ability": 12,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 20,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Deathmarked Hood",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 796,
+        "health": 450,
+        "offensive ability": 0,
+        "defensive ability": 18,
+        "elemental resist": 18,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 0,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+    {
+      "name": "Outcast's Secret",
+      "quality": "legendary",
+      "attributes": [
+        "armor": 598,
+        "health": 432,
+        "offensive ability": 45,
+        "defensive ability": 0,
+        "elemental resist": 0,
+        "fire resist": 0,
+        "cold resist": 0,
+        "lightning resist": 0,
+        "poison resist": 0,
+        "pierce resist": 0,
+        "bleed resist": 0,
+        "vitality resist": 0,
+        "aether resist": 28,
+        "chaos resist": 0,
+        "slow resist": 0,
+        "physical resist": 0
+      ]
+    }
+  ]
+};
